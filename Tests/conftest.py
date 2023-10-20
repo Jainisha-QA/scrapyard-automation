815d058ec5d879d86f5b3451ae904438d83f83b7
Jainisha-QA <148339420+Jainisha-QA@users.noreply.github.com>
2023-10-20 17:29:41 +0530

    Update conftest.py

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -3,11 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='class', autouse=True)
 def init_driver(request):
-    web_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    options = Options()
+    options.add.arhuments("--headless")
+    web_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     # web_driver = webdriver.Chrome(executable_path="https://chromedriver.storage.googleapis.com/index.html?path=114.0.5735.90/")
     request.cls.driver = web_driver
     web_driver.maximize_window()
